1b32ba346a5ba9f2173ae8e9621726d07f61a2fd
Dominique <Dominique@DESKTOP-LK5SI4Q>
2019-03-25 09:28:28 -0700

    front

--- src/main/resources/static/js/getcontroller.js
+++ src/main/resources/static/js/getcontroller.js
@@ -10,10 +10,10 @@ myApp.controller('DoubleController',  function($scope,$http,$log) {
 	  	$scope.cabs= response.data;
 	  	//$log.info(response);
 			  })};
-			
-				  
+			$scope.check='';
+			  $scope.chosencab={};
 			 $scope.search='false';
-			 $scope.customer={c_id: '',
+			 $scope.customer={
 				location: '',
 				destination : '',
 				number : '',
@@ -32,7 +32,10 @@ myApp.controller('DoubleController',  function($scope,$http,$log) {
 						  distance:''
 
 			 };
-
+			 $scope.add = function() {
+				    $http.post('/api/customer/add', $scope.customer).then(function(response) {console.log(response);});
+			 }
+			 
 $scope.add = function (){$http({
 	method:'POST',
     url:'/api/customer/add',
@@ -45,15 +48,27 @@ $scope.add = function (){$http({
 		})};
 
 		
-  
+ 
 
 $scope.showcab = function () {$http({
 	  method:'GET',
-		  url:'/api/customer/get'})
+		  url:'/api/customer/all'})
 		  .then(function(response)
 		  {
 	$scope.cabs= response.data;
 	//$log.info(response);
 		  })};
-		  
+		  $scope.show = function () {$http({
+			  method:'GET',
+				  url:'/api/cab/all'})
+				  .then(function(response)
+				  {
+		  	$scope.cabs= response.data;
+		  	//$log.info(response);
+				  })};
+			 $scope.pickup= function  (check)
+				  {
+					  customer.confirm_pk= 'true';
+				  };
+
 });
