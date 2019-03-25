bc1ebb63f13ed2681ba8e1ba910c4d0541dc556d
YungLord <Demar Johnson@LAPTOP-4IQFKNJD.mshome.net>
2019-03-25 15:17:58 -0500

    ntn serious

--- src/main/resources/static/js/getcontroller.js
+++ src/main/resources/static/js/getcontroller.js
@@ -2,60 +2,68 @@ var myApp = angular.module('myApp',[]);
 
 myApp.controller('DoubleController',  function($scope,$http,$log) {
  //$scope.cabs=[{}];
-  $scope.show = function () {$http({
-		  method:'GET',
-			  url:'/api/cab/all'})
-			  .then(function(response)
-			  {
-	  	$scope.cabs= response.data;
-	  	//$log.info(response);
-			  })};
+        $scope.show = function () {$http({
+                method:'GET',
+                    url:'/api/cab/all'})
+                    .then(function(response)
+                    {
+                $scope.cabs= response.data;
+                //$log.info(response);
+                    })};
 			  
-			  $scope.dest={
-					  location:'',
-					  destination:'',
-			  distance:'',
-			  price:'',
-			  };
+        $scope.dest={
+                location:'',
+                destination:'',
+                distance:'',
+                price:'',
+                };
 			 
 			$scope.check='';
 			  $scope.chosencab={};
 			 $scope.search='false';
 			 $scope.customer={
+                id: '',
 				location: '',
 				destination : '',
 				number : '',
 				feedback : '',
 				rating : '',
 				confirm_pk : '',
-				confirm_arr : ''};
+                confirm_arr : ''};
+                
 			 $scope.cab={
 					 c_id:'',
-						  trn:'',
-						  year:'',
-						  model:'',
-						  name:'',
-						  available:'',
-						  fare:'',
-						  distance:''
+                    trn:'',
+                    year:'',
+                    model:'',
+                    name:'',
+                    available:'',
+                    fare:'',
+                    distance:''
+                };
+            $scope.cabManager={
+                id: '',
+                email: '',
+                password: ''                                        
+            };
 
-			 };
-			 $scope.add = function() {
-				    $http.post('/api/customer/add', $scope.customer).then(function(response) {console.log(response);});
-			 }
+
+			//  $scope.add = function() {
+			// 	    $http.post('/api/customer/add', $scope.customer).then(function(response) {console.log(response);});
+			//  }
 			 
 $scope.add = function (){$http({
 	method:'POST',
     url:'/api/customer/add',
-    
 		data: $scope.customer
-	
 }).then(function(response)
 		{
 		console.log($scope.chosencab);
-		})};
-$scope.getreq= function()
-{	 $scope.request={
+        })};
+        
+$scope.getreq= function(){
+	$scope.request={
+        
 	id:	$scope.customer.id,
 	c_number: $scope.customer.number,
 	c_id: $scope.chosencab.c_id,
