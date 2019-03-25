ea3dac53cb6db200bd7feaeea7838831f2d7dbe5
Dominique <Dominique@DESKTOP-LK5SI4Q>
2019-03-25 09:07:58 -0700

    new fron end things added

--- src/main/resources/static/js/cabMan.js
+++ src/main/resources/static/js/cabMan.js
@@ -1,28 +1,44 @@
-var myApp = angular.module('myApp',[]);
 
-myApp.controller('DoubleController',  function($scope,$http,$log) {
- //$scope.cabs=[{}];
-  $scope.show = function () {$http({
-		  method:'GET',
-			  url:'/api/cab/all'})
-			  .then(function(response)
-			  {
-	  	$scope.cabs= response.data;
-	  	//$log.info(response);
-			  })};
-			
-				  
-			 $scope.search='false';  
-var ave='available';
-$scope.myFilter = function() {
- if ($scope.cabs.available == false)    
-   {
-   	return cabs;
-   	console.log(Scope.cabs);
-   }
- 
+var app = angular.module("modalFormApp", ['ui.bootstrap']);
+app.controller("modalAccountFormController", ['$scope', '$modal', '$log',
+
+    function ($scope, $modal, $log) {
+
+        $scope.showForm = function () {
+            $scope.message = "Show Form Button Clicked";
+            console.log($scope.message);
+
+            var modalInstance = $modal.open({
+                templateUrl: 'modal-form.html',
+                controller: ModalInstanceCtrl,
+                scope: $scope,
+                resolve: {
+                    userForm: function () {
+                        return $scope.userForm;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (selectedItem) {
+                $scope.selected = selectedItem;
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+        };
+            }]);
+
+var ModalInstanceCtrl = function ($scope, $modalInstance, userForm) {
+    $scope.form = {}
+    $scope.submitForm = function () {
+        if ($scope.form.userForm.$valid) {
+            console.log('user form is in scope');
+            $modalInstance.close('closed');
+        } else {
+            console.log('userform is not in scope');
+        }
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
 };
-
-		
-  
-});
--- src/main/resources/static/js/getcontroller.js
+++ src/main/resources/static/js/getcontroller.js
@@ -10,10 +10,10 @@ myApp.controller('DoubleController',  function($scope,$http,$log) {
 	  	$scope.cabs= response.data;
 	  	//$log.info(response);
 			  })};
-			
-				  
+			$scope.check='';
+			  $scope.chosencab={};
 			 $scope.search='false';
-			 $scope.customer={c_id: '',
+			 $scope.customer={
 				location: '',
 				destination : '',
 				number : '',
@@ -32,12 +32,15 @@ myApp.controller('DoubleController',  function($scope,$http,$log) {
 						  distance:''
 
 			 };
-
-/*$scope.add= function ){$http({
+			 $scope.add = function() {
+				    $http.post('/api/customer/add', $scope.customer).then(function(response) {console.log(response);});
+			 }
+			 
+$scope.add = function (){$http({
 	method:'POST',
-    url:'/api/customer/ad',
+    url:'/api/customer/add',
     
-		data: cab
+		data: $scope.customer
 	
 }).then(function(response)
 		{
@@ -45,17 +48,29 @@ myApp.controller('DoubleController',  function($scope,$http,$log) {
 		})};
 
 		
-  
+ 
 
 $scope.showcab = function () {$http({
 	  method:'GET',
-		  url:'/api/cab/all'})
+		  url:'/api/customer/all'})
 		  .then(function(response)
 		  {
 	$scope.cabs= response.data;
 	//$log.info(response);
-		  })};*/
-		  
+		  })};
+		  $scope.show = function () {$http({
+			  method:'GET',
+				  url:'/api/cab/all'})
+				  .then(function(response)
+				  {
+		  	$scope.cabs= response.data;
+		  	//$log.info(response);
+				  })};
+			 $scope.pickup= function  (check)
+				  {
+					  $scope.customer.confirm_pk='true';
+				  };
+
 });
 		
 			  
