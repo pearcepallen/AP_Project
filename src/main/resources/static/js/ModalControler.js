56eb233bbffe76e7bb15865a86597a4e526dd099
Dominique <Dominique@DESKTOP-LK5SI4Q>
2019-04-09 22:14:57 -0500

    sssss

---
+++ src/main/resources/static/js/ModalControler.js
@@ -0,0 +1,58 @@
+
+var myApp = angular.module('myApp',[]);
+myApp.directive('modalDialog', function () {
+    return {
+        restrict: 'E',
+        scope: {
+            show: '='
+        },
+        replace: true, // Replace with the template below
+        transclude: true, // we want to insert custom content inside the directive
+        link: function (scope, element, attrs) {
+            scope.dialogStyle = {};
+            if (attrs.width)
+                scope.dialogStyle.width = attrs.width;
+            if (attrs.height)
+                scope.dialogStyle.height = attrs.height;
+            scope.hideModal = function () {
+                scope.show = false;
+            };
+        },
+        template: "<div class='ng-modal' ng-show='show'><div class='ng-modal-overlay' ng-click='hideModal()'></div><div class='ng-modal-dialog' ng-style='dialogStyle'><div class='ng-modal-close' ng-click='hideModal()'>X</div><div class='ng-modal-dialog-content' ng-transclude></div></div></div>"
+    };
+});
+
+myApp.controller('MyCtrl', ['$scope', function ($scope,$window) {
+	
+	 $scope.Pass = true;
+     $scope.Fail = false;
+         // Login Usernames
+     var managerUsername= "topman";        
+
+         // Login Passwords
+      var managerPassword="topman";
+    $scope.modalShown = false;
+    $scope.toggleModal = function () {
+    	$scope.modalShown = !$scope.modalShown;                 
+         
+     
+    }
+    $scope
+
+	
+    $scope.login = function()
+                {
+                 
+                    //if ($scope.username  == managerUsername && $scope.password  == managerPassword )
+                      //  {
+                    	                             
+                          $scope.Pass = false;                                    
+                          return $window.alert("Hello " + $scope.username);
+                        //}
+//                    else
+//							  {
+                        //    return $scope.Fail = true;
+//                            } 
+                }                                              
+    
+}]);
